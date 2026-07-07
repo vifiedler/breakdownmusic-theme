465ac7a83df38724fb2725760a12a461ab881c78
vifiedler <vi.fiedler@duocuc.cl>
2026-07-07 17:31:26 -0400

    cambios en core para mostrarlos

--- assets/includes/modulos-functions.php
+++ assets/includes/modulos-functions.php
@@ -1,4 +1,4 @@
 <?php
-include get_template_directory().'/assets/modulos/modulo-video/core-modulo-cancion.php';
-include get_template_directory().'/assets/modulos/modulo-video/core-modulo-artista.php';
-include get_template_directory().'/assets/modulos/modulo-video/core-modulo-album.php';
+include get_template_directory().'/assets/modulos/modulo-cancion/core-modulo-cancion.php';
+include get_template_directory().'/assets/modulos/modulo-artista/core-modulo-artista.php';
+include get_template_directory().'/assets/modulos/modulo-album/core-modulo-album.php';
