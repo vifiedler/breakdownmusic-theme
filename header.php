093bc92a0dded284d795532ed2c11dbaf7cbe846
vifiedler <vi.fiedler@duocuc.cl>
2026-07-10 22:39:29 -0400

    menu otra ve

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
+ * @package breakdownmusic-theme
  */
 
 ?>
@@ -13,7 +14,7 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
 
@@ -24,23 +25,24 @@
     <?php wp_body_open(); ?>
     <div id="page" class="site">
         <a class="skip-link screen-reader-text"
-            href="#primary"><?php esc_html_e('Skip to content', 'breakdownmusic-theme'); ?></a>
-        <header id="masthead" class="fbs__net-navbar navbar navbar-expand-lg dark">
-            <div class="container-fluid d-flex align-items-center w-100 px-3">
-                <?php include get_template_directory() . '/assets/templates/navs/nav-desk.php'; ?>
-            </div>
-        </header>
-        <nav id="bd-sidebar" class="bd-sidebar-vertical">
+            href="#primary"><?php esc_html_e( 'Skip to content', 'breakdownmusic-theme' ); ?></a>
+            <header id="masthead" class="fbs__net-navbar navbar navbar-expand-lg dark">
+                <div class="container-fluid d-flex align-items-center justify-content-between">
+                    <?php include get_template_directory() . '/assets/templates/navs/nav-desk.php'; ?>
+                </div>
+        </header><!-- #masthead -->
+
+        <div class="offcanvas offcanvas-start bd-sidebar-vertical offcanvas-lg" tabindex="-1" id="bd-sidebar">
             <div class="bd-sidebar-menu-container">
                 <?php
                 wp_nav_menu(array(
                     'theme_location' => 'menu-superior',
-                    'menu_class' => 'bd-vertical-nav-list',
-                    'container' => 'false',
-                    'depth' => 2,
-                    'walker' => new bootstrap_5_wp_nav_menu_walker(),
-                    'fallback_cb' => 'bootstrap_5_wp_nav_menu_walker::fallback',
+                    'menu_class'     => 'bd-vertical-nav-list',
+                    'container'      => 'false',
+                    'depth'          => 2,
+                    'walker'         => new bootstrap_5_wp_nav_menu_walker(),
+                    'fallback_cb'    => 'bootstrap_5_wp_nav_menu_walker::fallback',
                 ));
                 ?>
             </div>
-        </nav>
+        </div>
