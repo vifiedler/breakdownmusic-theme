e4ddf6416291f34be8be0e9c63c55cd3759b208e
vifiedler <vi.fiedler@duocuc.cl>
2026-07-12 16:30:36 -0400

    Update taxonomy-genero_cancion.php

--- taxonomy-genero_cancion.php
+++ taxonomy-genero_cancion.php
@@ -16,19 +16,16 @@ get_header();
         <div class="row mb-4">
             <div class="col-12">
                 <h1 class="bd-archive-title"><?php echo esc_html($current_term->name); ?></h1>
-                <?php if ($current_term->description) : ?>
+                <?php if ($current_term->description): ?>
                     <p class="bd-archive-description"><?php echo esc_html($current_term->description); ?></p>
                 <?php endif; ?>
             </div>
         </div>
 
         <!-- Contenedor para las canciones (scroll infinito) -->
-        <div id="bd-genre-songs-container" 
-             data-genre="<?php echo esc_attr($current_term->slug); ?>" 
-             data-page="1" 
-             data-total-pages="0" 
-             data-orderby="title" 
-             data-order="ASC"><h2 class="d-none">Más canciones</h2>
+        <div id="bd-genre-songs-container" data-genre="<?php echo esc_attr($current_term->slug); ?>" data-page="1"
+            data-total-pages="0" data-orderby="title" data-order="ASC">
+            <h2 class="d-none">Más canciones</h2>
             <div id="bd-genre-songs-list">
                 <!-- Se llenará via AJAX -->
             </div>
